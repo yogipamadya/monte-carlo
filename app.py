3ce580a0cf2036ef53f2e81387675b172b3668a1
yogipamadya <31554546+yogipamadya@users.noreply.github.com>
2020-07-14 23:22:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,63 +1,116 @@
 import dash
+from dash.dependencies import Input, Output, State
+import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
+import pandas as pd
+import plotly.graph_objects as go
+import numpy as np
 
-########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='lightblue'
-color2='darkgreen'
-mytitle='Beer Comparison'
-tabtitle='beer!'
-myheading='Flying Dog Beers'
-label1='IBU'
-label2='ABV'
-githublink='https://github.com/austinlasseter/flying-dog-beers'
-sourceurl='https://www.flyingdog.com/beers/'
+tabel = {'input':['min','max','mean','stdev'],
+        'luas meter2':[1000,2000,1500,40],
+         'ketebalan meter':[1,2,3,4],
+         'ntg persen':[50,70,12,1],
+         'por persen':[10,20,15,2],
+         'sw persen':[30,50,20,5],
+         'bo':[1,1,1,1]}
 
-########### Set up the chart
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name=label1,
-    marker={'color':color1}
-)
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
-    name=label2,
-    marker={'color':color2}
-)
+iteration = {'iteration':[10000]}
 
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
-    barmode='group',
-    title = mytitle
-)
+df = pd.DataFrame(tabel)
+df2 = pd.DataFrame(iteration)
 
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
+app = dash.Dash(__name__)
 
 
-########### Initiate the app
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
-app.title=tabtitle
 
-########### Set up the layout
-app.layout = html.Div(children=[
-    html.H1(myheading),
-    dcc.Graph(
-        id='flyingdog',
-        figure=beer_fig
-    ),
-    html.A('Code on Github', href=githublink),
+app.layout = html.Div(
+[
+    html.H1('Simulasi Monte Carlo ver. 1 Beta'),
+    html.H2('Metode Distribusi Data: Truncated Normal Distribution'),
+    dcc.Markdown('''
+                 _Silahkan isi tabel dengan angka yang diinginkan_
+                 '''),
+    
+    html.Div([dash_table.DataTable(
+    id='table-input',
+    columns=[{"name": i, "id": i} for i in df.columns],
+    data=df.to_dict('records'),
+    editable=True,
+    )]),
+   
     html.Br(),
-    html.A('Data Source', href=sourceurl),
-    ]
+   
+    html.Div([dash_table.DataTable(
+    id='table-input2',
+    columns=[{"name": 'iteration', "id": 'iteration'}],
+    data=df2.to_dict('records'),
+    editable=True,
+    )]),
+   
+    html.Br(),
+   
+    dcc.Graph(id='grafik'),
+    
+    html.Br(),
+   
+    html.Div(id='deskripsi'),
+    
+    html.Br(),
+    
+    dcc.Markdown('''
+                  _yogipamadya_
+                  
+                  ''')
+       
+])
+ 
+@app.callback(
+    [Output('deskripsi','children'),Output('grafik','figure')],
+    [Input('table-input','data'),Input('table-input','columns'),Input('table-input2','data'),Input('table-input2','columns')]
 )
+def calculator(a,b,c,d):
+    df3 = pd.DataFrame(a,columns=[i['name'] for i in b])
+    df4 = pd.DataFrame(c,columns=[i['name'] for i in d])
+    
+    luas = np.random.normal(int(df3.iloc[2,1]),int(df3.iloc[3,1]),int(df4.iloc[0,0]))
+    luas2 = luas.clip(int(df3.iloc[0,1]),int(df3.iloc[1,1]))
+
+    ketebalan = np.random.normal(int(df3.iloc[2,2]),int(df3.iloc[3,2]),int(df4.iloc[0,0]))
+    ketebalan2 = ketebalan.clip(int(df3.iloc[0,2]),int(df3.iloc[1,2]))
+
+    ntg = np.random.normal(int(df3.iloc[2,3]),int(df3.iloc[3,3]),int(df4.iloc[0,0]))
+    ntg2 = ntg.clip(int(df3.iloc[0,3]),int(df3.iloc[1,3]))
+
+    por = np.random.normal(int(df3.iloc[2,4]),int(df3.iloc[3,4]),int(df4.iloc[0,0]))
+    por2 = por.clip(int(df3.iloc[0,4]),int(df3.iloc[1,4]))
+
+    sw = np.random.normal(int(df3.iloc[2,5]),int(df3.iloc[3,5]),int(df4.iloc[0,0]))
+    sw2 = sw.clip(int(df3.iloc[0,5]),int(df3.iloc[1,5]))
+
+    bo = np.random.uniform(int(df3.iloc[0,6]),int(df3.iloc[1,6]),int(df4.iloc[0,0]))
+
+    cadangan = 7758*(luas2*0.000247105)*(ketebalan2*3.28084)*(ntg2/100)*((100-sw2)/100)*(por2/100)/bo
+
+    total = pd.DataFrame(cadangan,columns=['OOIP']).describe(percentiles=[.1,.2,.25,.5,.6,.75,.9]).round().T
+    
+    fig = go.Figure()
+
+    fig.add_trace(go.Histogram(x=cadangan,cumulative_enabled=True))
+    fig.update_layout(
+    title_text='OOIP Iteration '+ str(df4.iloc[0,0]) , # title of plot
+    xaxis_title_text='Barrel Oil', # xaxis label
+    yaxis_title_text='Count', # yaxis label
+    )
+    des = [dash_table.DataTable(
+    id='tes',
+    columns=[{"name": i, "id": i} for i in total.columns],
+    data=total.to_dict('records'),
+    )]
+    
+    return (des,fig)
+    
+    
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
